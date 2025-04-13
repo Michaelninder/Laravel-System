097a09f7c9523a8778b3393aedd529f0a3d4cf73
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-13 20:14:41 +0200

    Create home.php

---
+++ resources/lang/en/pages/home.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'welcome_title' => 'Welcome to Laravel System',
+    'welcome_description' => 'This is your home page. You can customize it as needed.',
+    'logged_in_as' => 'You are logged in as :username.',
+    'go_to_dashboard' => 'Go to Dashboard',
+];
