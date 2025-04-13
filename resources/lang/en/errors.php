f64fec9b86363fb7cdf87aa647b5e9764dcd8801
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-13 19:13:08 +0200

    Create errors.php

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'back_home' => 'Back to Home',
+
+    '404' => [
+        'title' => 'Page Not Found',
+        'description' => 'Sorry, the page you are looking for does not exist.',
+    ],
+    '403' => [
+        'title' => 'Access Denied',
+        'description' => 'You do not have permission to view this page.',
+    ],
+    '419' => [
+        'title' => 'Page Expired',
+        'description' => 'Please refresh and try again.',
+    ],
+    '500' => [
+        'title' => 'Server Error',
+        'description' => 'Something went wrong on our side.',
+    ],
+    '503' => [
+        'title' => 'Service Unavailable',
+        'description' => 'We are currently undergoing maintenance.',
+    ],
+];
