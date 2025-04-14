1d236008f10a220553d5a03ded18c7caa5422793
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-14 15:07:41 +0200

    Create overview.php

---
+++ resources/lang/en/dashboard/overview.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Dashboard Overview',
+    'username' => 'Username',
+    'email' => 'Email',
+    'uuid' => 'User ID',
+    'role' => 'Role',
+    'created_at' => 'Member Since',
+    'balance' => 'Account Balance',
+];
