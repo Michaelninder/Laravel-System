53326d9b4db9fd99493ed812a93579fe2583fc47
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-14 18:22:42 +0200

    Create users.php

---
+++ resources/lang/en/admin/users.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'user_list' => 'User List',
+    'create_user' => 'Create User',
+    'edit_user' => 'Edit User',
+    'update_user' => 'Update User',
+    'user_created' => 'User has been created successfully.',
+    'user_updated' => 'User has been updated successfully.',
+    'username' => 'Username',
+    'email' => 'Email',
+    'password' => 'Password',
+    'password_confirmation' => 'Confirm Password',
+    'role' => 'Role',
+    'actions' => 'Actions',
+    'edit' => 'Edit',
+];
