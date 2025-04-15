6ec6cb8b4142c2690219f12a728ed5d6a82777f6
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-15 08:52:21 +0200

    Update PageController(added roules function)

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Rule;
 
 class PageController extends Controller
 {
-  public function home()
-  {
-	return view('pages.home');
-  }		
+    public function home()
+	{
+		return view('pages.home');
+	}		
+
+    public function rules()
+	{
+		$rules = Rules::orderBy('order_index')->get();
+		return view('pages.rules', compact('rules'));
+	}
 }
