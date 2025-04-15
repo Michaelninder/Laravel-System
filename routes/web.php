ba00393dcfd48ee0180ded9d9bc9940df24dfeac
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-15 08:39:47 +0200

    Update web.php (added rules routes)

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ErrorController;
 use App\Http\Controllers\Admin\PageController as AdminPageController;
 use App\Http\Controllers\Admin\UserController as AdminUserController;
-
-
+use App\Http\Controllers\Admin\RulesController as AdminRulesController; 
 
 Route::get('/set-locale/{locale}', function ($locale) {
     $langDirs = File::directories(resource_path('lang')); 
@@ -22,6 +21,8 @@ Route::get('/set-locale/{locale}', function ($locale) {
 });
 
 Route::get('/', [PageController::class, 'home'])->name('pages.home');
+Route::get('/rules', [PageController::class, 'rules'])->name('pages.rules');
+
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('auth.login');
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login.submit');
@@ -31,12 +32,14 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 Route::get('/auth', [AuthController::class, 'showLogin'])->name('login');
 
+
 Route::get('/dashboard', function () { return view('dashboard.overview'); })->name('dashboard.overview')->middleware('auth');
 
 Route::fallback(function () { return response()->view('errors.show', ['error' => 404], 404); });
 
 Route::get('/error/{error}', [ErrorController::class, 'show'])->name('errors.custom');
 	
+
 Route::middleware(['auth', 'check.admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/overview', [AdminPageController::class, 'overview'])->name('overview');
 	
@@ -45,4 +48,10 @@ Route::middleware(['auth', 'check.admin'])->prefix('admin')->name('admin.')->gro
     Route::post('/users', [AdminUserController::class, 'storeUser'])->name('users.store');
     Route::get('/users/{user}/edit', [AdminUserController::class, 'editUser'])->name('users.edit');
     Route::put('/users/{user}', [AdminUserController::class, 'updateUser'])->name('users.update');
+	
+    Route::get('/rules', [AdminRulesController::class, 'listRules'])->name('rules.index');
+    Route::get('/rules/create', [AdminRulesController::class, 'createRule'])->name('rules.create');
+    Route::post('/rules', [AdminRulesController::class, 'storeRule'])->name('rules.store');
+    Route::get('/rules/{rule}/edit', [AdminRulesController::class, 'editRule'])->name('rules.edit');
+    Route::put('/rules/{rule}', [AdminRulesController::class, 'updateRule'])->name('rules.update');
 });
