c9bc0e3bce5168d9b2d93a47423b03ef701ee9fa
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-15 08:38:40 +0200

    Create rules.php

---
+++ resources/lang/en/admin/rules.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Title',
+    'content' => 'Content',
+    'order_index' => 'Order Index',
+    'edit' => 'Edit',
+    'edit_rule' => 'Edit Rule',
+    'create_rule' => 'Create Rule',
+    'update_rule' => 'Update Rule',
+    'no_rules' => 'No rules found.',
+];
