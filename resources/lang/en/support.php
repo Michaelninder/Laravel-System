efa03a13440f7470187f357eba039a8662c3ed44
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-16 20:40:34 +0200

    Create support.php

---
+++ resources/lang/en/support.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'type_message' => 'Type your message...',
+    'send' => 'Send',
+    'ticket_information' => 'Ticket Information',
+    'subject' => 'Subject',
+    'status' => 'Status',
+    'user' => 'User',
+    'user_uuid' => 'User UUID',
+    'ticket_uuid' => 'Ticket UUID',
+    'created_at' => 'Created At',
+    'last_activity' => 'Last Activity',
+    'reopen_ticket' => 'Reopen Ticket',
+    'close_ticket' => 'Close Ticket',
+    'set_pending' => 'Set to Pending',
+    'status_updated' => 'Ticket status has been updated.',
+    'message_sent' => 'Message sent successfully.',
+];
