ec2eff3fd2d2fc3723cb4c06a1c1ec8a9fef2af6
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-16 19:20:34 +0200

    Create update_ticket_messages_table_add_user_uuid migration

---
+++ database/migrations/2025_04_16_171810_update_ticket_messages_table_add_user_uuid.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTicketMessagesTableAddUserUuid extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('support_messages', function (Blueprint $table) {
+            $table->uuid('user_uuid')->nullable()->after('ticket_uuid')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+
+    public function down()
+    {
+        Schema::table('support_messages', function (Blueprint $table) {
+            $table->dropColumn('user_uuid');
+        });
+    }
+}
