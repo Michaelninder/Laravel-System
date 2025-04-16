140f6627b51acf233190b5a02d9849be007bfe95
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-16 19:22:46 +0200

    Update and rename update_ticket_messages_table_add_user_uuid migration to update_support_messages_table_add_user_uuid migration

--- database/migrations/2025_04_16_171810_update_support_messages_table_add_user_uuid.php
+++ database/migrations/2025_04_16_171810_update_support_messages_table_add_user_uuid.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateTicketMessagesTableAddUserUuid extends Migration
+class UpdateSupportMessagesTableAddUserUuid extends Migration
 {
     /**
      * Run the migrations.
