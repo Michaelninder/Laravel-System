551db7b93df85143c18a7cbf58b3e0721c4308d9
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-16 15:32:12 +0200

    Create SupportTicket.php

---
+++ app/Models/SupportTicket.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+
+class SupportTicket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['uuid', 'user_uuid', 'subject', 'theme', 'status'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($ticket) {
+            $ticket->uuid = Str::uuid();
+        });
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(SupportMessage::class, 'ticket_uuid', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_uuid', 'uuid');
+    }
+}
