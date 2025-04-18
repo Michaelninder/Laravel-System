f5ada76f31b350a61027ad5cb1040922edb1b5aa
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-18 16:24:00 +0200

    Update forum.overview view

--- resources/views/forum/overview.blade.php
+++ resources/views/forum/overview.blade.php
@@ -8,25 +8,30 @@
     <div class="space-y-4">
         @forelse($forums as $forum)
             <div class="border border-gray-200 rounded p-4 bg-white flex justify-between items-start">
-                <div>
-                    <h2 class="text-lg font-medium text-gray-800">{{ $forum->title }}</h2>
-                    <p class="text-sm text-gray-600">{{ $forum->description }}</p>
-                </div>
-                @if(auth()->user()?->isAdmin())
-                    <div class="flex space-x-2">
-                        <a href="{{ route('forum.edit', $forum) }}" class="text-sm text-yellow-500 hover:text-yellow-600 flex items-center">
-                            <i class="bi bi-pencil-square mr-1"></i> {{ __('forum.edit') }}
-                        </a>
-                        <form method="POST" action="{{ route('forum.destroy', $forum) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" onclick="return confirm('{{ __('forum.delete_confirm') }}')" class="text-sm text-red-600 hover:text-red-700 flex items-center">
-                                <i class="bi bi-trash3-fill mr-1"></i> {{ __('forum.delete') }}
-                            </button>
-                        </form>
-                    </div>
-                @endif
-            </div>
+			    <div>
+			        <a href="{{ route('forum.view', $forum) }}" class="text-lg font-medium text-blue-700 hover:underline">
+			            {{ $forum->name }}
+			        </a>
+			        <p class="text-sm text-gray-600 mt-1">
+			            {{ $forum->description }}
+			        </p>
+			    </div>
+			
+			    @if(auth()->user()?->isAdmin())
+			        <div class="flex space-x-2">
+			            <a href="{{ route('forum.edit', $forum) }}" class="text-sm text-yellow-500 hover:text-yellow-600 flex items-center">
+			                <i class="bi bi-pencil-square mr-1"></i> {{ __('forum.edit') }}
+			            </a>
+			            <form method="POST" action="{{ route('forum.destroy', $forum->uuid) }}">
+			                @csrf
+			                @method('DELETE')
+			                <button type="submit" onclick="return confirm('{{ __('forum.delete_confirm') }}')" class="text-sm text-red-600 hover:text-red-700 flex items-center">
+			                    <i class="bi bi-trash3-fill mr-1"></i> {{ __('forum.delete') }}
+			                </button>
+			            </form>
+			        </div>
+			    @endif
+			</div>
         @empty
             <p class="text-sm text-gray-500">{{ __('forum.no_forums') }}</p>
         @endforelse
@@ -37,3 +42,5 @@
     </a>
 	@endif
 @endsection
+
+
