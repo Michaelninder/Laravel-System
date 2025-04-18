88002ee07c1b5389f2837dc9b1ee46ba1842a1ab
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-18 15:52:12 +0200

    Create create_forum_comments_table migration

---
+++ database/migrations/2025_04_17_125822_create_forum_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forum_comments', function (Blueprint $table) {
+		    $table->id();
+		    $table->uuid('uuid')->unique();
+		    $table->uuid('thread_uuid');
+		    $table->uuid('user_uuid');
+		    $table->text('body');
+		    $table->timestamps();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forum_comments');
+    }
+};
