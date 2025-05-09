c3280a03cfd5d35d80b8cbf1746db5864e28f0ab
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-09 08:13:05 +0200

    Update errors lang file add more errors

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -3,6 +3,18 @@
 return [
     'back_home' => 'Back to Home',
 
+    '400' => [
+        'title' => 'Bad Request',
+        'description' => 'The request could not be understood or was missing required parameters.',
+    ],
+    '401' => [
+        'title' => 'Unauthorized',
+        'description' => 'Authentication is required and has failed or has not yet been provided.',
+    ],
+    '402' => [
+        'title' => 'Payment Required',
+        'description' => 'Payment is required to access this resource.',
+    ],
     '404' => [
         'title' => 'Page Not Found',
         'description' => 'Sorry, the page you are looking for does not exist.',
@@ -11,16 +23,52 @@ return [
         'title' => 'Access Denied',
         'description' => 'You do not have permission to view this page.',
     ],
+    '405' => [
+        'title' => 'Method Not Allowed',
+        'description' => 'The requested method is not allowed for this resource.',
+    ],
+    '408' => [
+        'title' => 'Request Timeout',
+        'description' => 'The server timed out waiting for the request.',
+    ],
+    '409' => [
+        'title' => 'Conflict',
+        'description' => 'There was a conflict with the current state of the resource.',
+    ],
+    '410' => [
+        'title' => 'Gone',
+        'description' => 'The requested resource is no longer available.',
+    ],
     '419' => [
         'title' => 'Page Expired',
         'description' => 'Please refresh and try again.',
+    ],
+    '429' => [
+        'title' => 'Too Many Requests',
+        'description' => 'You have made too many requests in a short period.',
     ],
     '500' => [
         'title' => 'Server Error',
         'description' => 'Something went wrong on our side.',
     ],
+    '501' => [
+        'title' => 'Not Implemented',
+        'description' => 'This functionality is not available.',
+    ],
+    '502' => [
+        'title' => 'Bad Gateway',
+        'description' => 'Received an invalid response from the upstream server.',
+    ],
     '503' => [
         'title' => 'Service Unavailable',
         'description' => 'We are currently undergoing maintenance.',
+    ],
+    '504' => [
+        'title' => 'Gateway Timeout',
+        'description' => 'The upstream server failed to send a request in time.',
+    ],
+    '969' => [
+        'title' => 'Unknown Error',
+        'description' => 'Our service was not able to identify the error.',
     ],
 ];
